d997fa6ce4ba30632cf37af87d69d1ddc83c3663
SouroN21 <dasunpriyanaveen@gmail.com>
2025-01-11 00:21:45 +0530

    com2

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-50">
       <div className="w-full max-w-md mt-8">
-        <Photo />
+       {/*} <Photo />*/}
         <Social />
         <Name />
         <Contact />
