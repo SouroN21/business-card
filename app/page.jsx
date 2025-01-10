f21ef19afc2e918aca0c163757853ddc174d43bf
SouroN21 <dasunpriyanaveen@gmail.com>
2025-01-11 00:39:14 +0530

    photo

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Photo from "@/components/photo";
 import Social from "@/components/Social";
 import Name from "@/components/Name";
 import Contact from "@/components/Contact";
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-50">
       <div className="w-full max-w-md mt-8">
+        <Photo/>
         <Social />
         <Name />
         <Contact />
