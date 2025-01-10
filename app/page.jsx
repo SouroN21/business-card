223f4fc3c1bf3b457c5c1c8a32e3cc36c0b65a9f
SouroN21 <dasunpriyanaveen@gmail.com>
2025-01-11 00:21:03 +0530

    com

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Photo from "@/components/Photo";
+//import Photo from "@/components/Photo";
 import Social from "@/components/Social";
 import Name from "@/components/Name";
 import Contact from "@/components/Contact";
